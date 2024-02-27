c29f76eeef6a93ae8ecb6992c4279dace740e1ae
Aishwarya1544 <160617665+Aishwarya1544@users.noreply.github.com>
2024-02-27 14:53:29 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,127 @@
+print("hello world")
+number1 = 8
+name ="Aishwarya"
+print(name)
+num1 = 4.9
+print(type(num1))
+print(type(name))
+sample = float(3)
+print(sample)
+#strings
+name3 = "aishwarya is a good girl"
+print(len(name3))
+#getting the exact index 
+name0 = "helloworld"
+print(name0[0])
+#indexing
+print(name0[5:8])
+#3
+print(name0[::5])
+
+# Arithmetic Operators in Python
+
+# Addition
+num1 = 10
+num2 = 5
+result_add = num1 + num2
+print("Addition result:", result_add)  # Output: 15
+
+# Subtraction
+result_sub = num1 - num2
+print("Subtraction result:", result_sub)  # Output: 5
+
+# Multiplication
+result_mul = num1 * num2
+print("Multiplication result:", result_mul)  # Output: 50
+
+# Division
+result_div = num1 / num2
+print("Division result:", result_div)  # Output: 2.0
+
+# Floor Division
+result_floor_div = num1 // num2
+print("Floor Division result:", result_floor_div)  # Output: 2
+
+# Modulus (Remainder)
+result_mod = num1 % num2
+print("Modulus result:", result_mod)  # Output: 0
+
+# Exponentiation
+result_exp = num1 ** num2
+print("Exponentiation result:", result_exp)  # Output: 
+ # Logical Operators in Python
+
+# Logical AND
+x = True
+y = False
+result_and = x and y
+print("Logical AND result:", result_and)  # Output: False
+
+# Logical OR
+result_or = x or y
+print("Logical OR result:", result_or)  # Output: True
+
+# Logical NOT
+result_not_x = not x
+print("Logical NOT (x) result:", result_not_x)  # Output: False
+
+result_not_y = not y
+print("Logical NOT (y) result:", result_not_y)  # Output: True
+
+#while loop
+num = 1
+while num <= 5:
+    print(num)
+    num += 1
+#forloop
+    for i in range(5):
+        print(i)
+
+# Bitwise AND
+x = 10 
+y = 6
+result_and = x & y
+print("Bitwise AND result:", result_and)  # Output: 2
+
+# Bitwise OR
+result_or = x | y
+print("Bitwise OR result:", result_or)    # Output: 14
+
+# Bitwise XOR
+result_xor = x ^ y
+print("Bitwise XOR result:", result_xor)  # Output: 12
+
+# Bitwise NOT
+result_not_x = ~x
+print("Bitwise NOT (x) result:", result_not_x)  # Output: -11
+
+# Bitwise left Shift
+result_left_shift = x << 1
+print("Bitwise Left Shift result:", result_left_shift)  # Output: 20
+
+# Bitwise right Shift
+result_right_shift = x >> 1
+print("Bitwise Right Shift result:", result_right_shift)  # Output: 5
+
+# Comparison Operators
+a = 5
+b = 10
+
+# >
+print("Greater Than:", a > b)   # Output: False
+
+# <
+print("Less Than:", a < b)      # Output: True
+
+#>=
+print("Greater Than or Equal To:", a >= b)   # Output: False
+
+#<=
+print("Less Than or Equal To:", a <= b)      # Output: True
+
+# ==
+print("Equal To:", a == b)     # Output: False
+
+#!=
+print("Not Equal To:", a != b)   # Output: True
+
